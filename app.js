7366213ad8486e5a353050b5df250fd96a491c3c
Hammad-official-2520 <106146562+Hammad-official-2520@users.noreply.github.com>
2022-08-09 21:09:32 +0500

    assignment commentbox

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+function comment(){
+    document.getElementById('commm').innerHTML += (
+        `<div id="com-x">
+            <div class="pro-com"><img class="profile"  src="./image/varun.png" alt="">` + document.getElementById('my-post').value + `</div>     
+        </div>`
+    )
+     
+ }
+function comment2(){
+    document.getElementById("com-y").innerHTML += (
+ `<div id="com-y">                  
+        <div class="pro-com"><img class="profile"  src="./image/elon.jpg" alt="">` +document.getElementById("my").value + `</div>
+        </div>`
+    )
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
